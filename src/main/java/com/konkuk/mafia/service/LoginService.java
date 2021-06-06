67c55a08e28459b80338b6fc86ece92c16c011e5
hottinggot <hottinggot@naver.com>
2021-06-06 19:58:40 +0900

    Login response format 수정

--- src/main/java/com/konkuk/mafia/service/LoginService.java
+++ src/main/java/com/konkuk/mafia/service/LoginService.java
@@ -3,5 +3,9 @@ package com.konkuk.mafia.service;
 import com.konkuk.mafia.dto.Users;
 
 public interface LoginService {
-    boolean findUser(Users users) throws Exception;
+
+    enum STATUS {
+        ID_NOT_EXISTS, PASSWD_ERR, SUCCESS
+    }
+    STATUS findUser(Users users) throws Exception;
 }
